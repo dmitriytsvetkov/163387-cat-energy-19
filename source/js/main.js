5ced8613460754f9a0f6bf65f4c8dd5642c626cf
tsvetkovNotebook <dstvetkov@gmail.com>
2020-05-24 23:03:25 +0300

    Добавил сборку проекта

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,7 @@
-const navMain = document.querySelector('.main-nav');
-const navToggle = document.querySelector('.main-nav__toggle');
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
+var catalogBtn = document.querySelectorAll('.product-card__link');
+var showMoreBtn = document.querySelector('.show-more-block__show-all');
 
 navMain.classList.remove('main-nav--nojs');
 
@@ -11,4 +13,16 @@ navToggle.addEventListener('click', function () {
     navMain.classList.remove('main-nav--opened');
     navMain.classList.add('main-nav--closed')
   }
+})
+
+for (var i = 0; i < catalogBtn.length; i++) {
+  catalogBtn[i].addEventListener("click", function openPopup(evt) {
+    evt.preventDefault();
+    alert("Модальное окно");
+  })
+}
+
+showMoreBtn.addEventListener("click", function getProducts(evt) {
+  evt.preventDefault();
+  alert("Ajax загрузка товаров");
 })
